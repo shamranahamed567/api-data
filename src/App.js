c92b6702311fbfe0ebea8be7ea891dc5db0cdd0b
Ahamed Shamran <shamranahamed567@gmail.com>
2023-02-02 16:17:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import "./App.css";
+import useFetch from "./useFetch";
 
 function App() {
+  const { data, loading, error } = useFetch("https://v2.jokeapi.dev/joke/Any");
+
+  if (loading) return <h2>Loading ...</h2>;
+
+  if (error) console.log(error);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-box">
+      <div className="first-box">
+        <h2 className="heading-secondary">{data?.category}</h2>
+        <p className="description">{data?.setup}</p>
+      </div>
+      <div className="first-box">
+        <h2 className="heading-secondary">{data?.category}</h2>
+        <p className="description">{data?.setup}</p>
+      </div>
+      <div className="first-box">
+        <h2 className="heading-secondary">{data?.category}</h2>
+        <p className="description">{data?.setup}</p>
+      </div>
     </div>
   );
 }
